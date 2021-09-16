a3c737d9283c47629ff30e73d294d3db8ffe59ac
Daniel Ward <dward1502@gmail.com>
2021-09-15 21:16:15 -0700

    added sections to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,28 @@
 import {Fragment} from 'react'
-// import Image from 'next/image'
+import Image from 'next/image'
 // import {CgArrowRightR} from 'react-icons/cg'
 
-// import styles from '../sass/Home.module.scss'
+import styles from '../sass/Home.module.scss'
 
 export default function Home() {
+  const heroImg = '/images/heart&ekgTransparent.webp'
   return (
     <Fragment>
-      
+      <section className={styles.heroText}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugiat animi itaque aut quia ipsa, laboriosam optio voluptatibus voluptates consectetur accusantium architecto placeat rerum quaerat? Sapiente minima a repellendus quasi!</p>
+      </section>
+      <div className={styles.heroImgCont}>
+        <Image src={heroImg} alt='Heart with EKG line' layout='fill'/> 
+      </div>
+      <section className={styles.stories}>
+        <h1>TRANSPLANT STORIES</h1>
+        <div >
+
+        </div>
+      </section>
+      <section className={styles.blog}>
+        <h1>BI-WEEKLY BLOG</h1>
+      </section>
     </Fragment>
   );
 }
