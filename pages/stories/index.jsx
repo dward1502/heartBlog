13056d2517ddbfd45a235895d7530c9c40f2bd96
Daniel Ward <dward1502@gmail.com>
2021-09-20 12:34:29 -0700

    worked on stories page

--- pages/stories/index.jsx
+++ pages/stories/index.jsx
@@ -1,9 +1,113 @@
-const StoriesPage = () => {
-    return (
-        <div>
-           stories 
-        </div>
-    )
-}
+import { Fragment } from 'react';
+import Image from 'next/image';
 
-export default StoriesPage
+import styles from './stories.module.scss';
+
+const StoriesPage = () => {
+  return (
+    <Fragment>
+      <section className={styles.hero}>
+        <div className={styles.box}>
+          <div className={styles.heroImg}>
+            <Image
+              src='/images/neverGiveUp.png'
+              alt='Never give up ekg with heart photo'
+              layout='fill'
+            />
+          </div>
+          <div className={styles.heroText}>
+            <p>
+              Transplant patients have a bond with each other that many people
+              can not understand, even our loved ones. We have all at some time
+              been close to death before our transplant only to be called and
+              given the gift, suddenly our life has been changed drasticly. This
+              sudden change is dealth with differently by every patient and here
+              I thought it would be a good place for use to describe our stories
+              and share our adventures.
+            </p>
+            <p>
+              Anyone can add a story regarding their journey all that needs to
+              be done is sign up / login and the ADD STORY button will be
+              available. There you will fill out your story, add a picture and
+              post the story for others to read and see. Comment on stories that
+              move you, and hopefully we can grow with our experience as well as
+              others
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className={styles.title}>
+        <h1>OUR STORIES</h1>
+        <button>ADD STORY</button>
+      </div>
+      <section className={styles.dualStoriesGrid}>
+        <div className={styles.mainCard}>
+          <div className={styles.imgCont}></div>
+          <div className={styles.mainText}>
+            <h1>Title</h1>
+            <h3>09/05/2021</h3>
+            <h5>Username</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              corrupti earum quam eius officia ad? Cupiditate consectetur veniam
+              eos laboriosam quas voluptatum repellat minus voluptate assumenda
+              recusandae? Hic consequatur, labore maxime laboriosam
+              exercitationem sed eligendi alias harum numquam iste? Velit nam
+              deserunt sunt necessitatibus accusamus voluptates aut expedita!
+              Dicta, quo.
+            </p>
+            <div className={styles.readMore}>
+              <button className={styles.btn}>Read More</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.mainCard}>
+          <div className={styles.imgCont}></div>
+          <div className={styles.mainText}>
+            <h1>Title</h1>
+            <h3>09/05/2021</h3>
+            <h5>Username</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              corrupti earum quam eius officia ad? Cupiditate consectetur veniam
+              eos laboriosam quas voluptatum repellat minus voluptate assumenda
+              recusandae? Hic consequatur, labore maxime laboriosam
+              exercitationem sed eligendi alias harum numquam iste? Velit nam
+              deserunt sunt necessitatibus accusamus voluptates aut expedita!
+              Dicta, quo.
+            </p>
+            <div className={styles.readMore}>
+              <button className={styles.btn}>Read More</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.threeStoriesGrid}>
+        <div className={`${styles.one} ${styles.mainCard}`}>
+          <div className={styles.imgCont}></div>
+          <div className={styles.mainText}>
+            <h1>Title</h1>
+            <h3>09/05/2021</h3>
+            <h5>Username</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              corrupti earum quam eius officia ad? Cupiditate consectetur veniam
+              eos laboriosam quas voluptatum repellat minus voluptate assumenda
+              recusandae? Hic consequatur, labore maxime laboriosam
+              exercitationem sed eligendi alias harum numquam iste? Velit nam
+              deserunt sunt necessitatibus accusamus voluptates aut expedita!
+              Dicta, quo.
+            </p>
+            <div className={styles.readMore}>
+              <button className={styles.btn}>Read More</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.two}></div>
+        <div className={styles.three}></div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default StoriesPage;
